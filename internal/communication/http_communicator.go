525e3adba4f5eefc1ab6452b9578c46f19cab4c3
Anish <anishm7030@gmail.com>
2025-06-10 21:28:09 -0400

    added function to stop http communicator

--- internal/communication/http_communicator.go
+++ internal/communication/http_communicator.go
@@ -1,11 +1,13 @@
 package communication
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"sync"
+	"time"
 )
 
 type HTTPCommunicator struct {
@@ -40,6 +42,12 @@ func (c *HTTPCommunicator) Start() error {
 	}()
 
 	return nil
+}
+
+func (c *HTTPCommunicator) Stop() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	return c.httpServer.Shutdown(ctx)
 }
 
 func (c *HTTPCommunicator) handleMessage(w http.ResponseWriter, r *http.Request) {
