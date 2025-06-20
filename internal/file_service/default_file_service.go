63df1f28a3eae7ce4d097c8f3a09de4b779d4ecd
Anish <anishm7030@gmail.com>
2025-06-19 21:14:46 -0400

    added a default file service

---
+++ internal/file_service/default_file_service.go
@@ -0,0 +1,11 @@
+package file_service
+
+import (
+	"github.com/AnishMulay/sandstore/internal/chunk_service"
+	"github.com/AnishMulay/sandstore/internal/metadata_service"
+)
+
+type DefaultFileService struct {
+	ms metadata_service.MetadataService
+	cs chunk_service.ChunkService
+}
