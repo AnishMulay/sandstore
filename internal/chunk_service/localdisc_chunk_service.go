44301ca95322b90639326ccb9827a8873e763e2d
Anish <anishm7030@gmail.com>
2025-06-19 20:54:06 -0400

    added chunk disc functions for local disc service

--- internal/chunk_service/localdisc_chunk_service.go
+++ internal/chunk_service/localdisc_chunk_service.go
@@ -19,3 +19,18 @@ func NewLocalDiscChunkService(baseDir string) *LocalDiscChunkService {
 func (cs *LocalDiscChunkService) chunkPath(chunkID string) string {
 	return filepath.Join(cs.baseDir, chunkID+".chunk")
 }
+
+func (cs *LocalDiscChunkService) WriteChunk(chunkID string, data []byte) error {
+	path := cs.chunkPath(chunkID)
+	return os.WriteFile(path, data, os.ModePerm)
+}
+
+func (cs *LocalDiscChunkService) ReadChunk(chunkID string) ([]byte, error) {
+	path := cs.chunkPath(chunkID)
+	return os.ReadFile(path)
+}
+
+func (cs *LocalDiscChunkService) DeleteChunk(chunkID string) error {
+	path := cs.chunkPath(chunkID)
+	return os.Remove(path)
+}
