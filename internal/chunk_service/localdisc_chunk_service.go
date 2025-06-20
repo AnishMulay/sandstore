ef5215ebb1e9b02b2201d54452700479ff3dadb9
Anish <anishm7030@gmail.com>
2025-06-19 20:31:36 -0400

    added a function to find a chunk in on a disc

--- internal/chunk_service/localdisc_chunk_service.go
+++ internal/chunk_service/localdisc_chunk_service.go
@@ -1,5 +1,21 @@
 package chunk_service
 
+import (
+	"os"
+	"path/filepath"
+)
+
 type LocalDiscChunkService struct {
 	baseDir string
+}
+
+func NewLocalDiscChunkService(baseDir string) *LocalDiscChunkService {
+	if err := os.MkdirAll(baseDir, os.ModePerm); err != nil {
+		panic(err)
+	}
+	return &LocalDiscChunkService{baseDir: baseDir}
+}
+
+func (cs *LocalDiscChunkService) chunkPath(chunkID string) string {
+	return filepath.Join(cs.baseDir, chunkID+".chunk")
 }
