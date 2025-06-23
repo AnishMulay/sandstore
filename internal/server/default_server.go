85205cdafa955934945b4e3af72f3e67d1b43c70
Anish <anishm7030@gmail.com>
2025-06-22 22:35:19 -0400

    fixed errors in main.go and added new defualt server creation function

--- internal/server/default_server.go
+++ internal/server/default_server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/AnishMulay/sandstore/internal/communication"
 	"github.com/AnishMulay/sandstore/internal/file_service"
@@ -12,4 +13,31 @@ type DefaultServer struct {
 	fs     file_service.FileService
 	ctx    context.Context
 	cancel context.CancelFunc
+}
+
+func NewDefaultServer(comm communication.Communicator, fs file_service.FileService) *DefaultServer {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &DefaultServer{
+		comm:   comm,
+		fs:     fs,
+		ctx:    ctx,
+		cancel: cancel,
+	}
+}
+
+func (s *DefaultServer) Start() {
+	s.comm.Start(s.handleMessage)
+}
+
+func (s *DefaultServer) Stop() {
+	s.comm.Stop()
+	s.cancel()
+}
+
+func (s *DefaultServer) handleMessage(msg communication.Message) (*communication.Response, error) {
+	fmt.Printf("Received message: %v\n", msg)
+	return &communication.Response{
+		Code: communication.CodeOK,
+		Body: []byte("OK"),
+	}, nil
 }
