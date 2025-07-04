770982de31d6b5337840a4c06838355b3130213c
Anish <anishm7030@gmail.com>
2025-07-04 19:51:57 -0400

    added functions to in memory node registry implementation

--- internal/node_registry/inmemory_node_registry.go
+++ internal/node_registry/inmemory_node_registry.go
@@ -9,3 +9,28 @@ func NewInMemoryNodeRegistry() *InMemoryNodeRegistry {
 		nodes: []Node{},
 	}
 }
+
+func (r *InMemoryNodeRegistry) RegisterNode(node Node) error {
+	r.nodes = append(r.nodes, node)
+	return nil
+}
+
+func (r *InMemoryNodeRegistry) DeregisterNode(node Node) error {
+	for i, n := range r.nodes {
+		if n.ID == node.ID {
+			r.nodes = append(r.nodes[:i], r.nodes[i+1:]...)
+			return nil
+		}
+	}
+	return nil
+}
+
+func (r *InMemoryNodeRegistry) GetHealthyNodes() ([]Node, error) {
+	var healthyNodes []Node
+	for _, node := range r.nodes {
+		if node.Healthy {
+			healthyNodes = append(healthyNodes, node)
+		}
+	}
+	return healthyNodes, nil
+}
