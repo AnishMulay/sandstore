2f39d4039e194d8db4d4080c2a9361005f6cb62e
Anish <anishm7030@gmail.com>
2025-07-04 17:18:46 -0400

    updated grpc communicator implementation to use any payload type

--- internal/communication/grpc_communicator.go
+++ internal/communication/grpc_communicator.go
@@ -1,122 +1,161 @@
 package communication
 
-// import (
-// 	"context"
-// 	"fmt"
-// 	"log"
-// 	"net"
-// 	"sync"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"reflect"
+	"sync"
 
-// 	communicationpb "github.com/AnishMulay/sandstore/gen/proto/communication"
+	communicationpb "github.com/AnishMulay/sandstore/gen/proto/communication"
 
-// 	"google.golang.org/grpc"
-// 	"google.golang.org/grpc/credentials/insecure"
-// )
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
 
-// type GRPCCommunicator struct {
-// 	listenAddress string
-// 	handler       MessageHandler
-// 	grpcServer    *grpc.Server
+type GRPCCommunicator struct {
+	listenAddress string
+	handler       MessageHandler
+	grpcServer    *grpc.Server
 
-// 	clientLock sync.RWMutex
-// 	clients    map[string]communicationpb.MessageServiceClient
-// }
+	clientLock   sync.RWMutex
+	clients      map[string]communicationpb.MessageServiceClient
+	payloadTypes map[string]reflect.Type
+}
 
-// func NewGRPCCommunicator(addr string) *GRPCCommunicator {
-// 	return &GRPCCommunicator{
-// 		listenAddress: addr,
-// 		clients:       make(map[string]communicationpb.MessageServiceClient),
-// 	}
-// }
+func NewGRPCCommunicator(addr string) *GRPCCommunicator {
+	c := &GRPCCommunicator{
+		listenAddress: addr,
+		clients:       make(map[string]communicationpb.MessageServiceClient),
+		payloadTypes:  make(map[string]reflect.Type),
+	}
 
-// func (c *GRPCCommunicator) Address() string {
-// 	return c.listenAddress
-// }
+	// Register default payload types
+	c.payloadTypes[MessageTypeStoreFile] = reflect.TypeOf((*StoreFileRequest)(nil)).Elem()
+	c.payloadTypes[MessageTypeReadFile] = reflect.TypeOf((*ReadFileRequest)(nil)).Elem()
+	c.payloadTypes[MessageTypeDeleteFile] = reflect.TypeOf((*DeleteFileRequest)(nil)).Elem()
 
-// func (c *GRPCCommunicator) Start(handler MessageHandler) error {
-// 	c.handler = handler
-// 	c.grpcServer = grpc.NewServer()
-// 	communicationpb.RegisterMessageServiceServer(c.grpcServer, &grpcServer{comm: c})
+	return c
+}
 
-// 	lis, err := net.Listen("tcp", c.listenAddress)
-// 	if err != nil {
-// 		return err
-// 	}
+func (c *GRPCCommunicator) Address() string {
+	return c.listenAddress
+}
 
-// 	go func() {
-// 		if err := c.grpcServer.Serve(lis); err != nil {
-// 			log.Printf("GRPC server error: %v", err)
-// 		}
-// 	}()
-// 	return nil
-// }
+func (c *GRPCCommunicator) Start(handler MessageHandler) error {
+	c.handler = handler
+	c.grpcServer = grpc.NewServer()
+	communicationpb.RegisterMessageServiceServer(c.grpcServer, &grpcServer{comm: c})
 
-// func (c *GRPCCommunicator) Stop() error {
-// 	c.grpcServer.GracefulStop()
-// 	return nil
-// }
+	lis, err := net.Listen("tcp", c.listenAddress)
+	if err != nil {
+		return err
+	}
 
-// func (c *GRPCCommunicator) Send(ctx context.Context, to string, msg Message) (*Response, error) {
-// 	c.clientLock.RLock()
-// 	client, ok := c.clients[to]
-// 	c.clientLock.RUnlock()
+	go func() {
+		if err := c.grpcServer.Serve(lis); err != nil {
+			log.Printf("GRPC server error: %v", err)
+		}
+	}()
+	return nil
+}
 
-// 	if !ok {
-// 		conn, err := grpc.NewClient(to, grpc.WithTransportCredentials(insecure.NewCredentials()))
-// 		if err != nil {
-// 			return nil, fmt.Errorf("failed to create gRPC client: %w", err)
-// 		}
-// 		client = communicationpb.NewMessageServiceClient(conn)
-// 		c.clientLock.Lock()
-// 		c.clients[to] = client
-// 		c.clientLock.Unlock()
-// 	}
+func (c *GRPCCommunicator) Stop() error {
+	c.grpcServer.GracefulStop()
+	return nil
+}
 
-// 	req := &communicationpb.MessageRequest{
-// 		From:    msg.From,
-// 		Type:    msg.Type,
-// 		Payload: msg.Payload,
-// 	}
+func (c *GRPCCommunicator) Send(ctx context.Context, to string, msg Message) (*Response, error) {
+	c.clientLock.RLock()
+	client, ok := c.clients[to]
+	c.clientLock.RUnlock()
 
-// 	resp, err := client.SendMessage(ctx, req)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to send message: %w", err)
-// 	}
+	if !ok {
+		conn, err := grpc.NewClient(to, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			return nil, fmt.Errorf("failed to create gRPC client: %w", err)
+		}
+		client = communicationpb.NewMessageServiceClient(conn)
+		c.clientLock.Lock()
+		c.clients[to] = client
+		c.clientLock.Unlock()
+	}
 
-// 	return &Response{
-// 		Code:    SandCode(resp.Code),
-// 		Body:    resp.Body,
-// 		Headers: resp.Headers,
-// 	}, nil
-// }
+	// Serialize payload to JSON bytes
+	var payloadBytes []byte
+	if msg.Payload != nil {
+		var err error
+		payloadBytes, err = json.Marshal(msg.Payload)
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal payload: %w", err)
+		}
+	}
 
-// type grpcServer struct {
-// 	communicationpb.UnimplementedMessageServiceServer
-// 	comm *GRPCCommunicator
-// }
+	req := &communicationpb.MessageRequest{
+		From:    msg.From,
+		Type:    msg.Type,
+		Payload: payloadBytes,
+	}
 
-// func (s *grpcServer) SendMessage(ctx context.Context, req *communicationpb.MessageRequest) (*communicationpb.MessageResponse, error) {
-// 	if s.comm.handler == nil {
-// 		return nil, fmt.Errorf("handler not set")
-// 	}
+	resp, err := client.SendMessage(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send message: %w", err)
+	}
 
-// 	msg := Message{
-// 		From:    req.From,
-// 		Type:    req.Type,
-// 		Payload: req.Payload,
-// 	}
-// 	resp, err := s.comm.handler(msg)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	return &Response{
+		Code:    SandCode(resp.Code),
+		Body:    resp.Body,
+		Headers: resp.Headers,
+	}, nil
+}
 
-// 	if resp == nil {
-// 		return &communicationpb.MessageResponse{Code: "OK"}, nil
-// 	}
+type grpcServer struct {
+	communicationpb.UnimplementedMessageServiceServer
+	comm *GRPCCommunicator
+}
 
-// 	return &communicationpb.MessageResponse{
-// 		Code:    string(resp.Code),
-// 		Body:    resp.Body,
-// 		Headers: resp.Headers,
-// 	}, nil
-// }
+func (s *grpcServer) SendMessage(ctx context.Context, req *communicationpb.MessageRequest) (*communicationpb.MessageResponse, error) {
+	if s.comm.handler == nil {
+		return nil, fmt.Errorf("handler not set")
+	}
+
+	msg := Message{
+		From: req.From,
+		Type: req.Type,
+	}
+
+	// Deserialize payload from JSON bytes
+	if payloadType, exists := s.comm.payloadTypes[req.Type]; exists {
+		if len(req.Payload) > 0 {
+			// Create a new instance of the payload type
+			payloadPtr := reflect.New(payloadType).Interface()
+
+			if err := json.Unmarshal(req.Payload, payloadPtr); err != nil {
+				return nil, fmt.Errorf("failed to unmarshal payload for type %s: %w", req.Type, err)
+			}
+
+			// Dereference the pointer to get the actual value
+			msg.Payload = reflect.ValueOf(payloadPtr).Elem().Interface()
+		} else {
+			// Empty payload - create zero value
+			msg.Payload = reflect.Zero(payloadType).Interface()
+		}
+	}
+
+	resp, err := s.comm.handler(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp == nil {
+		return &communicationpb.MessageResponse{Code: "OK"}, nil
+	}
+
+	return &communicationpb.MessageResponse{
+		Code:    string(resp.Code),
+		Body:    resp.Body,
+		Headers: resp.Headers,
+	}, nil
+}
