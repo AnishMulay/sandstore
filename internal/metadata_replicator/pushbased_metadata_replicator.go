75c4066d798cc2d31f6ebc2cceae6ca8d3172967
Anish <anishm7030@gmail.com>
2025-07-07 22:28:01 -0400

    metadata replication to all healthy nodes for push based replicator

--- internal/metadata_replicator/pushbased_metadata_replicator.go
+++ internal/metadata_replicator/pushbased_metadata_replicator.go
@@ -1,6 +1,10 @@
 package metadata_replicator
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/AnishMulay/sandstore/internal/communication"
 	"github.com/AnishMulay/sandstore/internal/metadata_service"
 	"github.com/AnishMulay/sandstore/internal/node_registry"
@@ -19,5 +23,30 @@ func NewPushBasedMetadataReplicator(nr node_registry.NodeRegistry, comm communic
 }
 
 func (mr *PushBasedMetadataReplicator) ReplicateMetadata(metadata metadata_service.FileMetadata) error {
+	nodes, err := mr.nodeRegistry.GetHealthyNodes()
+	if err != nil {
+		return err
+	}
+
+	for _, node := range nodes {
+		msg := communication.Message{
+			From:    mr.comm.Address(),
+			Type:    communication.MessageTypeStoreMetadata,
+			Payload: metadata,
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		resp, err := mr.comm.Send(ctx, node.Address, msg)
+		if err != nil {
+			return err
+		}
+
+		if resp.Code != communication.CodeOK {
+			return fmt.Errorf("failed to replicate metadata to node %s: %s", node.ID, resp.Body)
+		}
+	}
+
 	return nil
 }
