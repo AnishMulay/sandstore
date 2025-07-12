2c174a5d35974c1b39121923b290e72b8ea3d9e7
Anish <anishm7030@gmail.com>
2025-07-12 18:43:35 -0400

    added log service logging to default chunk service

--- internal/chunk_service/localdisc_chunk_service.go
+++ internal/chunk_service/localdisc_chunk_service.go
@@ -27,16 +27,70 @@ func (cs *LocalDiscChunkService) chunkPath(chunkID string) string {
 }
 
 func (cs *LocalDiscChunkService) WriteChunk(chunkID string, data []byte) error {
+	cs.ls.Info(log_service.LogEvent{
+		Message: "Writing chunk",
+		Metadata: map[string]any{"chunkID": chunkID, "size": len(data)},
+	})
+	
 	path := cs.chunkPath(chunkID)
-	return os.WriteFile(path, data, os.ModePerm)
+	err := os.WriteFile(path, data, os.ModePerm)
+	if err != nil {
+		cs.ls.Error(log_service.LogEvent{
+			Message: "Failed to write chunk",
+			Metadata: map[string]any{"chunkID": chunkID, "error": err.Error()},
+		})
+		return err
+	}
+	
+	cs.ls.Info(log_service.LogEvent{
+		Message: "Chunk written successfully",
+		Metadata: map[string]any{"chunkID": chunkID},
+	})
+	return nil
 }
 
 func (cs *LocalDiscChunkService) ReadChunk(chunkID string) ([]byte, error) {
+	cs.ls.Info(log_service.LogEvent{
+		Message: "Reading chunk",
+		Metadata: map[string]any{"chunkID": chunkID},
+	})
+	
 	path := cs.chunkPath(chunkID)
-	return os.ReadFile(path)
+	data, err := os.ReadFile(path)
+	if err != nil {
+		cs.ls.Error(log_service.LogEvent{
+			Message: "Failed to read chunk",
+			Metadata: map[string]any{"chunkID": chunkID, "error": err.Error()},
+		})
+		return nil, err
+	}
+	
+	cs.ls.Info(log_service.LogEvent{
+		Message: "Chunk read successfully",
+		Metadata: map[string]any{"chunkID": chunkID, "size": len(data)},
+	})
+	return data, nil
 }
 
 func (cs *LocalDiscChunkService) DeleteChunk(chunkID string) error {
+	cs.ls.Info(log_service.LogEvent{
+		Message: "Deleting chunk",
+		Metadata: map[string]any{"chunkID": chunkID},
+	})
+	
 	path := cs.chunkPath(chunkID)
-	return os.Remove(path)
+	err := os.Remove(path)
+	if err != nil {
+		cs.ls.Error(log_service.LogEvent{
+			Message: "Failed to delete chunk",
+			Metadata: map[string]any{"chunkID": chunkID, "error": err.Error()},
+		})
+		return err
+	}
+	
+	cs.ls.Info(log_service.LogEvent{
+		Message: "Chunk deleted successfully",
+		Metadata: map[string]any{"chunkID": chunkID},
+	})
+	return nil
 }
