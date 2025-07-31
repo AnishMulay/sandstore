1e7b8fede5dfdd3ba8ddbac961cbedf1c9c5ec83
Anish <anishm7030@gmail.com>
2025-07-31 15:13:07 -0400

    assigned integer levels to logging levels

--- internal/log_service/log_service.go
+++ internal/log_service/log_service.go
@@ -9,6 +9,13 @@ const (
 	ErrorLevel = "ERROR"
 )
 
+const (
+	DebugLevelValue = 1
+	InfoLevelValue  = 2
+	WarnLevelValue  = 3
+	ErrorLevelValue = 4
+)
+
 type LogEvent struct {
 	Timestamp time.Time
 	NodeID    string
@@ -21,4 +28,19 @@ type LogService interface {
 	Info(event LogEvent)
 	Warn(event LogEvent)
 	Error(event LogEvent)
+}
+
+func GetLevelValue(level string) int {
+	switch level {
+	case DebugLevel:
+		return DebugLevelValue
+	case InfoLevel:
+		return InfoLevelValue
+	case WarnLevel:
+		return WarnLevelValue
+	case ErrorLevel:
+		return ErrorLevelValue
+	default:
+		return InfoLevelValue // Default to INFO if unknown
+	}
 }
