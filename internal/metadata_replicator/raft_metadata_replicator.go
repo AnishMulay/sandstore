c630effac8e6f51f2395b907b31bfef3eb6c2db2
Anish <anishm7030@gmail.com>
2025-08-03 21:10:25 -0400

    added func to create raft metadata replicator

---
+++ internal/metadata_replicator/raft_metadata_replicator.go
@@ -0,0 +1,41 @@
+package metadata_replicator
+
+import (
+	"context"
+	"sync"
+
+	"github.com/AnishMulay/sandstore/internal/cluster_service"
+	"github.com/AnishMulay/sandstore/internal/communication"
+	"github.com/AnishMulay/sandstore/internal/log_service"
+)
+
+type RaftMetadataReplicator struct {
+	clusterService cluster_service.ClusterService
+	comm           communication.Communicator
+	ls             log_service.LogService
+
+	metadataLog *MetadataLog
+
+	mu sync.RWMutex
+
+	// pending ops for now, might change to something simpler later
+	pendingOps  map[int64]chan error
+	nextOpIndex int64
+
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+func NewRaftMetadataReplicator(clusterService cluster_service.ClusterService, comm communication.Communicator, ls log_service.LogService) *RaftMetadataReplicator {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &RaftMetadataReplicator{
+		clusterService: clusterService,
+		comm:           comm,
+		ls:             ls,
+		metadataLog:    NewMetadataLog(),
+		pendingOps:     make(map[int64]chan error),
+		nextOpIndex:    0,
+		ctx:            ctx,
+		cancel:         cancel,
+	}
+}
