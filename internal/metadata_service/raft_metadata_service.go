6673dcca7fecefe0d1274d5fa915f296087bbcdf
Anish <anishm7030@gmail.com>
2025-08-03 20:43:09 -0400

    added struct for allowed metadata operations, create and delete only for now

--- internal/metadata_service/raft_metadata_service.go
+++ internal/metadata_service/raft_metadata_service.go
@@ -16,3 +16,16 @@ const (
 	OpTypeCreate MetadataOpType = "create"
 	OpTypeDelete MetadataOpType = "delete"
 )
+
+type MetadataOperation struct {
+	CreateOp *CreateMetadataOp `json:"create_op,omitempty"`
+	DeleteOp *DeleteMetadataOp `json:"delete_op,omitempty"`
+}
+
+type CreateMetadataOp struct {
+	Metadata FileMetadata `json:"metadata"`
+}
+
+type DeleteMetadataOp struct {
+	Path string `json:"path"`
+}
