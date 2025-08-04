a93e8a46547724e26a08ed775ba6538110dccac2
Anish <anishm7030@gmail.com>
2025-08-03 20:46:40 -0400

    added helper functions to workwith the metadata log

--- internal/metadata_service/raft_metadata_service.go
+++ internal/metadata_service/raft_metadata_service.go
@@ -47,3 +47,50 @@ func NewMetadataLog() *MetadataLog {
 		lastApplied: 0,
 	}
 }
+
+func (ml *MetadataLog) AppendEntry(entry MetadataLogEntry) int64 {
+	ml.mu.Lock()
+	defer ml.mu.Unlock()
+
+	entry.Index = int64(len(ml.entries)) + 1
+	ml.entries = append(ml.entries, entry)
+	return entry.Index
+}
+
+func (ml *MetadataLog) GetEntries(startIndex int64) []MetadataLogEntry {
+	ml.mu.RLock()
+	defer ml.mu.RUnlock()
+
+	if startIndex <= 0 || startIndex > int64(len(ml.entries)) {
+		return []MetadataLogEntry{}
+	}
+
+	return ml.entries[startIndex-1:]
+}
+
+func (ml *MetadataLog) GetLastLogIndex() int64 {
+	ml.mu.RLock()
+	defer ml.mu.RUnlock()
+	return int64(len(ml.entries))
+}
+
+func (ml *MetadataLog) GetLastLogTerm() int64 {
+	ml.mu.RLock()
+	defer ml.mu.RUnlock()
+
+	if len(ml.entries) == 0 {
+		return 0
+	}
+	return ml.entries[len(ml.entries)-1].Term
+}
+
+func (ml *MetadataLog) GetEntryAtIndex(index int64) *MetadataLogEntry {
+	ml.mu.RLock()
+	defer ml.mu.RUnlock()
+
+	if index <= 0 || index > int64(len(ml.entries)) {
+		return nil
+	}
+
+	return &ml.entries[index-1]
+}
