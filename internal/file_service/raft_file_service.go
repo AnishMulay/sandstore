fe78c0392651586d07d194922c6f316912f1585b
Anish <anishm7030@gmail.com>
2025-08-06 11:55:59 -0400

    refactored store file func in raft file service

--- internal/file_service/raft_file_service.go
+++ internal/file_service/raft_file_service.go
@@ -1,18 +1,28 @@
 package file_service
 
 import (
-	"github.com/yourusername/yourproject/internal/log_service"
+	"crypto/sha256"
+	"fmt"
+	"time"
+
+	"github.com/AnishMulay/sandstore/internal/chunk_replicator"
+	"github.com/AnishMulay/sandstore/internal/chunk_service"
+	"github.com/AnishMulay/sandstore/internal/log_service"
+	"github.com/AnishMulay/sandstore/internal/metadata_replicator"
+	"github.com/AnishMulay/sandstore/internal/metadata_service"
+	"github.com/google/uuid"
 )
 
 type RaftFileService struct {
-	ls log_service.LogService
-	mr metadata_replicator.RaftMetadataReplicator
-	cs chunk_service.ChunkService
-	ms metadata_service.MetadataService
-	cr chunk_replicator.ChunkReplicator
+	ls        log_service.LogService
+	mr        *metadata_replicator.RaftMetadataReplicator
+	cs        chunk_service.ChunkService
+	ms        metadata_service.MetadataService
+	cr        chunk_replicator.ChunkReplicator
 	chunkSize int64
 }
 
+// right now the chunks are created and replicated first. but this will also change once an update metadata op is added
 func (fs *RaftFileService) StoreFile(path string, data []byte) error {
 	fs.ls.Info(log_service.LogEvent{
 		Message:  "Storing file (RaftFileService)",
@@ -47,7 +57,7 @@ func (fs *RaftFileService) StoreFile(path string, data []byte) error {
 			return ErrChunkStoreFailed
 		}
 
-		replicas, err := fs.cr.ReplicateChunk(chunkID, chunkData, 2) // Assuming replication factor of 3
+		replicas, err := fs.cr.ReplicateChunk(chunkID, chunkData, 2)
 		if err != nil {
 			fs.ls.Error(log_service.LogEvent{
 				Message:  "Failed to replicate chunk",
@@ -78,36 +88,45 @@ func (fs *RaftFileService) StoreFile(path string, data []byte) error {
 		Metadata: map[string]any{"path": path, "fileID": fileID, "numChunks": len(chunks)},
 	})
 
-	metadata := metadata_service.NewFileMetadata(path, int64(len(data)), chunks)
-	err := fs.ms.CreateFileMetadataFromStruct(metadata)
-	if err != nil {
-		fs.ls.Error(log_service.LogEvent{
-			Message:  "Failed to create file metadata",
-			Metadata: map[string]any{"path": path, "error": err.Error()},
-		})
-		return ErrMetadataCreateFailed
-	}
+	// Create temporary metadata struct for Raft replication
+	tempMetadata := metadata_service.NewFileMetadata(path, int64(len(data)), chunks)
 
 	fs.ls.Debug(log_service.LogEvent{
-		Message:  "Replicating metadata",
+		Message:  "Replicating metadata via Raft",
 		Metadata: map[string]any{"path": path},
 	})
 
+	// Replicate via Raft first - only proceed if consensus is achieved
 	op := metadata_replicator.MetadataReplicationOp{
 		Type:     metadata_replicator.CREATE,
-		Metadata: metadata,
+		Metadata: tempMetadata,
 	}
-	err = fs.mr.Replicate(op)
+	err := fs.mr.Replicate(op)
 	if err != nil {
 		fs.ls.Error(log_service.LogEvent{
-			Message:  "Failed to replicate metadata",
+			Message:  "Failed to replicate metadata via Raft",
 			Metadata: map[string]any{"path": path, "error": err.Error()},
 		})
 		return ErrMetadataReplicationFailed
 	}
 
+	fs.ls.Debug(log_service.LogEvent{
+		Message:  "Raft replication successful, committing to local store",
+		Metadata: map[string]any{"path": path},
+	})
+
+	// Only commit to local metadata store after Raft consensus
+	err = fs.ms.CreateFileMetadataFromStruct(tempMetadata)
+	if err != nil {
+		fs.ls.Error(log_service.LogEvent{
+			Message:  "Failed to commit file metadata to local store",
+			Metadata: map[string]any{"path": path, "error": err.Error()},
+		})
+		return ErrMetadataCreateFailed
+	}
+
 	fs.ls.Info(log_service.LogEvent{
-		Message:  "File stored successfully",
+		Message:  "File stored successfully via Raft",
 		Metadata: map[string]any{"path": path, "chunks": len(chunks)},
 	})
 
