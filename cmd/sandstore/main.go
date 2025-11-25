828a97495b246b88685a14b0d35488ae97c3afb1
AnishMulay <anishm7030@gmail.com>
2025-11-25 18:07:04 -0500

    added support for POSIX server type in main function and updated command-line flags

--- cmd/sandstore/main.go
+++ cmd/sandstore/main.go
@@ -5,18 +5,19 @@ import (
 	"log"
 	"strings"
 
-	"github.com/AnishMulay/sandstore/servers/raft"
-	"github.com/AnishMulay/sandstore/servers/simple"
+	"[github.com/AnishMulay/sandstore/servers/posix](https://github.com/AnishMulay/sandstore/servers/posix)"
+	"[github.com/AnishMulay/sandstore/servers/raft](https://github.com/AnishMulay/sandstore/servers/raft)"
+	"[github.com/AnishMulay/sandstore/servers/simple](https://github.com/AnishMulay/sandstore/servers/simple)"
 )
 
 func main() {
 	var (
-		serverType = flag.String("server", "raft", "Server type (raft)")
+		serverType = flag.String("server", "raft", "Server type (raft, simple, posix)")
 		nodeID     = flag.String("node-id", "", "Node ID")
 		listen     = flag.String("listen", ":8080", "Listen address")
 		dataDir    = flag.String("data-dir", "./data", "Data directory")
 		seeds      = flag.String("seeds", "", "Comma-separated seed peers")
-		bootstrap  = flag.Bool("bootstrap", false, "Bootstrap cluster")
+		bootstrap  = flag.Bool("bootstrap", false, "Bootstrap cluster (legacy raft only)")
 	)
 	flag.Parse()
 
@@ -49,6 +50,17 @@ func main() {
 			DataDir:    *dataDir,
 		}
 		server := simple.Build(opts)
+		if err := server.Run(); err != nil {
+			log.Fatalf("Server failed: %v", err)
+		}
+	case "posix":
+		opts := posix.Options{
+			NodeID:     *nodeID,
+			ListenAddr: *listen,
+			DataDir:    *dataDir,
+			SeedPeers:  seedPeers,
+		}
+		server := posix.Build(opts)
 		if err := server.Run(); err != nil {
 			log.Fatalf("Server failed: %v", err)
 		}
